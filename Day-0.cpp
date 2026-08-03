c44f9aa7c9e0cb0890e6a3dc3cfb8e15a91457d9
Md Rashidul Islam <rashid6788g@gmail.com>
2026-08-03 23:38:24 +0600

    Create Day-0.cpp

---
+++ Day-0.cpp
@@ -0,0 +1,15 @@
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string i;  
+    cout << "Hello, World." << endl;
+    getline(cin, i);
+    cout << i << endl;
+    return 0;
+}
